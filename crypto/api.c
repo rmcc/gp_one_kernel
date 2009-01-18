f3e8c72bd0bb9c03544205adf8a61eb5fe9b6764
Dave Jones <davej@redhat.com>
2009-01-17 22:28:42 -0500

    [CPUFREQ] Stupidly trivial CodingStyle fix
    GNU indent complains about this being ambiguous, because it's dumb.
    One of my automated tests relies on the output of indent, so this shuts
    it up.
    
    Signed-off-by: Dave Jones <davej@redhat.com>

--- crypto/api.c
+++ crypto/api.c
@@ -244,7 +244,7 @@ struct crypto_alg *crypto_alg_mod_lookup(const char *name, u32 type, u32 mask)
 	struct crypto_alg *larval;
 	int ok;
 
-	if (!((type | mask) & CRYPTO_ALG_TESTED)) {
+	if (!(mask & CRYPTO_ALG_TESTED)) {
 		type |= CRYPTO_ALG_TESTED;
 		mask |= CRYPTO_ALG_TESTED;
 	}
@@ -453,8 +453,8 @@ err:
 }
 EXPORT_SYMBOL_GPL(crypto_alloc_base);
 
-void *crypto_create_tfm(struct crypto_alg *alg,
-			const struct crypto_type *frontend)
+struct crypto_tfm *crypto_create_tfm(struct crypto_alg *alg,
+				     const struct crypto_type *frontend)
 {
 	char *mem;
 	struct crypto_tfm *tfm = NULL;
@@ -488,9 +488,9 @@ out_free_tfm:
 		crypto_shoot_alg(alg);
 	kfree(mem);
 out_err:
-	mem = ERR_PTR(err);
+	tfm = ERR_PTR(err);
 out:
-	return mem;
+	return tfm;
 }
 EXPORT_SYMBOL_GPL(crypto_create_tfm);
 
@@ -514,11 +514,12 @@ EXPORT_SYMBOL_GPL(crypto_create_tfm);
  *
  *	In case of error the return value is an error pointer.
  */
-void *crypto_alloc_tfm(const char *alg_name,
-		       const struct crypto_type *frontend, u32 type, u32 mask)
+struct crypto_tfm *crypto_alloc_tfm(const char *alg_name,
+				    const struct crypto_type *frontend,
+				    u32 type, u32 mask)
 {
 	struct crypto_alg *(*lookup)(const char *name, u32 type, u32 mask);
-	void *tfm;
+	struct crypto_tfm *tfm;
 	int err;
 
 	type &= frontend->maskclear;
