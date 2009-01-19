13e7c6343d39b6e76e85e3297e1a32d33b64e13b
Takashi Iwai <tiwai@suse.de>
2009-01-19 14:30:48 +0100

    ALSA: ca0106 - Add quirk for GA-G1975X mobo
    Giga-byte GA-G1975X mobo has a CA0106 on-board chip.
    Reference: bnc#395807
    	https://bugzilla.novell.com/show_bug.cgi?id=395807
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/isa/opl3sa2.c
+++ sound/isa/opl3sa2.c
@@ -179,13 +179,12 @@ static unsigned char __snd_opl3sa2_read(struct snd_opl3sa2 *chip, unsigned char 
 	unsigned char result;
 #if 0
 	outb(0x1d, port);	/* password */
-	printk(KERN_DEBUG "read [0x%lx] = 0x%x\n", port, inb(port));
+	printk("read [0x%lx] = 0x%x\n", port, inb(port));
 #endif
 	outb(reg, chip->port);	/* register */
 	result = inb(chip->port + 1);
 #if 0
-	printk(KERN_DEBUG "read [0x%lx] = 0x%x [0x%x]\n",
-	       port, result, inb(port));
+	printk("read [0x%lx] = 0x%x [0x%x]\n", port, result, inb(port));
 #endif
 	return result;
 }
@@ -234,10 +233,7 @@ static int __devinit snd_opl3sa2_detect(struct snd_card *card)
 		snd_printk(KERN_ERR PFX "can't grab port 0x%lx\n", port);
 		return -EBUSY;
 	}
-	/*
-	snd_printk(KERN_DEBUG "REG 0A = 0x%x\n",
-		   snd_opl3sa2_read(chip, 0x0a));
-	*/
+	// snd_printk("REG 0A = 0x%x\n", snd_opl3sa2_read(chip, 0x0a));
 	chip->version = 0;
 	tmp = snd_opl3sa2_read(chip, OPL3SA2_MISC);
 	if (tmp == 0xff) {
