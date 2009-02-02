25be6dd25e5c8c40a621a5f94ef19c1bfc4853ff
Kay Sievers <kay.sievers@vrfy.org>
2009-02-02 21:08:30 +0100

    mmc: struct device - replace bus_id with dev_name(), dev_set_name()
    Acked-by: Greg Kroah-Hartman <gregkh@suse.de>
    Signed-off-by: Kay Sievers <kay.sievers@vrfy.org>
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>

--- kernel/printk.c
+++ kernel/printk.c
@@ -1292,11 +1292,8 @@ EXPORT_SYMBOL(printk_ratelimit);
 bool printk_timed_ratelimit(unsigned long *caller_jiffies,
 			unsigned int interval_msecs)
 {
-	if (*caller_jiffies == 0
-			|| !time_in_range(jiffies, *caller_jiffies,
-					*caller_jiffies
-					+ msecs_to_jiffies(interval_msecs))) {
-		*caller_jiffies = jiffies;
+	if (*caller_jiffies == 0 || time_after(jiffies, *caller_jiffies)) {
+		*caller_jiffies = jiffies + msecs_to_jiffies(interval_msecs);
 		return true;
 	}
 	return false;
